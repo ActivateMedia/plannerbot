5b82b4c07b4ce94a4813d6b838541aebbf714ccb
rexromae <afalzettidroid@gmail.com>
2016-03-09 14:49:47 +0000

    Event timezones conversion (London/Geneva/Delhi) + E Location + Notes

--- index.js
+++ index.js
@@ -4,27 +4,117 @@ var xmljs = require("libxmljs");
 var express = require('express');
 var caldav = require("node-caldav");
 var moment = require('moment-timezone');
+
+/* Initialising Slack Client */
+Slack = require('node-slackr');
+slack = new Slack(config.slack.webhook_url,{
+  channel: config.slack.channel,
+  username: config.slack.username,
+  icon_emoji: config.slack.emoji
+});
+
+/* Initialising Express APP */
 var app = express();
 
+/* Root API Endpoint */
 app.get('/', function (req, res) {
  res.send('Activate Media Planner!');
  console.log(moment().day());
 });
 
+/* Today Events API Endpoint */
 app.get('/get-today-events', function (req, res) {
 
-  getTodayEvents(function(data) {
-     console.log(data);
-     postTodayEvents(function(result) {
-         console.log(result);
+  getTodayEvents(function(events) {
+     postTodayEvents(events, function(result) {
+         console.log("Slack message has been sent");
+	 res.send("Slack message sent successfully");
      });    
   });
 });
 
-function postTodayEvents(cb) {
+var findPropertyNameByRegex = function(o, r) {
+  var key;
+  for (key in o) {
+    if (key.match(r)) {
+      return key;
+    }
+  }
+  return undefined;
+};
 
+function postTodayEvents(events, cb) {
+
+  var messages = {
+    text: "Good morning <!channel|channel>! Here the events for today:",
+    channel: config.slack.channel,
+    attachments: [
+      {
+        fallback: "",
+        color: "good", // Can either be one of 'good', 'warning', 'danger'
+        fields: []
+      }
+    ]
+  };
+
+  events.forEach(function(event) {
+   
+    console.log(event);    
+    var eventLabels = "";
+    var startDate = findPropertyNameByRegex(event, "DTSTART");
+    var startDateLabel = "";
+    if(event[startDate].length == 8) {
+      //es 20160309
+      startDateLabel = "All day";
+    } else {
+      var endDate = findPropertyNameByRegex(event, "DTEND");
+      var _m1 = moment(event[startDate]);
+      var _m2 = moment(event[endDate]);
+      var ukTime = ":uk: " + _m1.tz("Europe/London").format('h:mm a') + " - " + _m2.tz("Europe/London").format('h:mm a');
+      var swissTime = ":flag-ch: " + _m1.tz("Europe/Zurich").format('h:mm a') + " - " + _m2.tz("Europe/Zurich").format('h:mm a');
+      var delhiTime = ":flag-in: " + _m1.tz("Asia/Colombo").format('h:mm a') + " - " + _m2.tz("Asia/Colombo").format('h:mm a');
+      startDateLabel = ukTime + " / " + swissTime + " / " + delhiTime;
+    }
+    eventLabels += startDateLabel;
+    var locationLabel = "";
+    if(typeof event['LOCATION'] !== "undefined") {
+       locationLabel = event['LOCATION'];
+    }
+    if(locationLabel.length > 0) {
+      eventLabels += "\n:pushpin: " + locationLabel;
+    }
+    
+    var notesLabel = "";
+    if(typeof event['DESCRIPTION'] !== "undefined") {
+       notesLabel = event["DESCRIPTION"];
+    }
+    if(notesLabel.length > 0) {
+      eventLabels += "\n:pencil2: " + notesLabel;
+    }
+
+    var _tmpObj = { 
+        fallback: "fallback text",
+        color: "good", // Can either be one of 'good', 'warning', 'danger'
+        fields: [{              
+                  title: event.SUMMARY,
+                  value: eventLabels,
+                  short: false
+                 }]
+    }; 
+    messages.attachments.push(_tmpObj);
+    console.log(event);
+  });
+
+  slack.notify(messages, function(err, result) {
+    console.log(err, result);
+  });
+
+  cb(true);
 }
 
+/*
+ * This function retrieves the events from the calendar and return an array of objects
+ */
 function getTodayEvents(cb) {
   var query_start_date = moment().subtract(1, 'days').set('hour', 23).set('minute', 59).set('second', 59).format(config.caldav.timeFormat) + "Z";
  var query_end_date = moment().set('hour', 23).set('minute', 59).set('second', 59).format(config.caldav.timeFormat) + "Z";
@@ -37,8 +127,9 @@ function getTodayEvents(cb) {
  });
 }
 
-
-
+/*
+ * Main Express Process 
+ */
 app.listen(3000, function () {
  console.log(config.app.name + ' listening on port ' + config.api.port);
 });
